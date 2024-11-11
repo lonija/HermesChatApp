6ff887bc48e6142b0004bf3f25e453952ccad359
lonija <127788426+lonija@users.noreply.github.com>
2024-11-11 18:29:38 +0200

    Add files via upload

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using HermesChat.Data.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HermesChat.Web.Controllers
+{
+    public class ChatController : Controller
+    {
+        //private readonly HermesChatContext _context;
+
+        //public ChatController(HermesChatContext context)
+        //{
+        //    _context = context;
+        //}
+        //[Authorize]
+        //public async Task<IActionResult> Chat()
+        //{
+        //    var chats = await _context.Chat.ToListAsync();
+        //    return View(chats);
+        //}
+
+
+    }
+}
